d9a4464134b3449b69d9be19296b3fbd6c77ad6b
Sidney-Tan <624124600@qq.com>
2024-11-06 01:26:02 +0800

    disable scope for indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,6 +3,10 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
     main = "ibl",
-    opts = {}
+    opts = {
+      scope = {
+        enabled = false,
+      },
+    },
   },
 }
