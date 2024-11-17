85cf2bee165ca4e4cf7e90a217a49eb341a3a5f4
Sidney-Tan <624124600@qq.com>
2024-11-17 16:27:48 +0800

    modify config of neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,8 +1,10 @@
 return {
   {
     "karb94/neoscroll.nvim",
+    event = "VeryLazy",
     opts = {
-      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      --mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      mappings = { "<C-u>", "<C-d>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
       hide_cursor = true,          -- Hide cursor while scrolling
       stop_eof = true,             -- Stop at <EOF> when scrolling downwards
       respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
