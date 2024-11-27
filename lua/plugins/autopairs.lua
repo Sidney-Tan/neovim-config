b7df2444298dd42ee338ebdf0c10d60cede4e0a4
Sidney-Tan <624124600@qq.com>
2024-11-27 14:23:30 +0800

    modify config for autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -4,8 +4,8 @@ return {
     event = "InsertEnter",
     opts = {
       -- Conflict with coc#pum#confirm, so set map_cr=false.
-      -- Don't need to map it even without a coc.
-      map_cr = false,
+      -- Set mar_cr=true when use build-in lsp
+      map_cr = true,
     },
   },
 }
