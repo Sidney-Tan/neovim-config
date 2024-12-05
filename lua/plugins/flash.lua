1cb37b5d1b158f56e1dd3d73ee547361f64625c5
Sidney-Tan <624124600@qq.com>
2024-12-05 22:30:53 +0800

    update keybinding

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
     opts = {},
     keys = {
       -- "s" is conflict with nvim-surround when use "ys", so map it to <CR>
-      { "<CR>",      mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "<BS>",      mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
       { "S",         mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
       { "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
       { "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
