d7b2f72564cbaac44eb7e97997b8111176e7976c
Sidney-Tan <624124600@qq.com>
2025-03-31 15:35:20 +0800

    install mini.icon and update lazy-lock.json

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "stevearc/dressing.nvim",
+    lazy = true,
+  },
+
+  {
+    "ziontee113/icon-picker.nvim",
+    lazy = true,
+    dependencies = { "stevearc/dressing.nvim" },
+    config = function()
+      require("icon-picker").setup {
+        disable_legacy_commands = true,
+      }
+    end,
+    keys = {
+      { "<leader>ip", "<cmd>IconPickerInsert<CR>", mode = { "n", "i" }, desc = "Icon Picker Insert" },
+    },
+  },
+
+  { "echasnovski/mini.icons", lazy = true, version = "*" },
+
+  {
     "nvim-tree/nvim-web-devicons",
     lazy = true,
     --[[
